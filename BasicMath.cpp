75494c1c382ec65dfca7068f4d485cae8da120b3
Viviana Hurtado <viviana.hurtado-antiporta@stu.ocsb.ca>
2025-02-18 07:25:47 +0000

    Adedded BasicMath.cpp

---
+++ BasicMath.cpp
@@ -0,0 +1,16 @@
+// Copyright 2025 Viviana Hurtado
+//   Name: Viviana Hurtado
+//  Date: 12/02/25
+//  Course: ICS3U Computer Science
+//  Description: this program does basic math calculations
+#include<iostream>
+#include<cmath>
+
+int main() {
+    std::cout << "9+4=" << (9+4) << std::endl;
+    std::cout << "5-2=" << (5-2) << std::endl;
+    std::cout << "8/2=" << (8/2) << std::endl;
+    std::cout << "9+3/2=" << (9+3/2) << std::endl;
+    std::cout << "4+5*2=" << (4+5*2) << std::endl;
+    std::cout << "3+pow(2, 3)=" << (3+pow(2, 3)) << std::endl;
+}
